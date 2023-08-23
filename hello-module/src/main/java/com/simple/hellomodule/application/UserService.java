899a1123233d85826173b6d73fcd4ec579a27709
Jeong Myeonggu <audrn6689@gmail.com>
2023-08-23 21:25:45 +0900

    init

--- hello-module/src/main/java/com/simple/hellomodule/application/UserService.java
+++ hello-module/src/main/java/com/simple/hellomodule/application/UserService.java
@@ -1,6 +1,5 @@
 package com.simple.hellomodule.application;
 
-import com.simple.hellomodule.infrastructure.LogHandler;
 import com.simple.hellomodule.domain.User;
 import com.simple.hellomodule.domain.UserRepository;
 import org.springframework.stereotype.Service;
@@ -9,22 +8,16 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final LogHandler logHandler;
 
-    public UserService(UserRepository userRepository, LogHandler logHandler) {
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.logHandler = logHandler;
     }
 
     public User findUserById(Long userId) {
-        logHandler.logInfo("find user with ID: " + userId);
-
         return userRepository.findById(userId).orElseThrow();
     }
 
     public void deleteUserById(Long userId) {
-        logHandler.logInfo("delete user with ID: " + userId);
-
         userRepository.deleteById(userId);
     }
 }
