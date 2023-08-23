74b1d8169bfb552d0b30268bb82cb47e386795e2
Jeong Myeonggu <audrn6689@gmail.com>
2023-08-23 16:51:47 +0900

    init

--- hello-module/src/main/java/com/simple/hellomodule/application/UserService.java
+++ hello-module/src/main/java/com/simple/hellomodule/application/UserService.java
@@ -1,6 +1,6 @@
 package com.simple.hellomodule.application;
 
-import com.simple.hellomodule.common.LoggingService;
+import com.simple.hellomodule.infrastructure.LogHandler;
 import com.simple.hellomodule.domain.User;
 import com.simple.hellomodule.domain.UserRepository;
 import org.springframework.stereotype.Service;
@@ -9,21 +9,21 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final LoggingService loggingService;
+    private final LogHandler logHandler;
 
-    public UserService(UserRepository userRepository, LoggingService loggingService) {
+    public UserService(UserRepository userRepository, LogHandler logHandler) {
         this.userRepository = userRepository;
-        this.loggingService = loggingService;
+        this.logHandler = logHandler;
     }
 
     public User findUserById(Long userId) {
-        loggingService.logInfo("find user with ID: " + userId);
+        logHandler.logInfo("find user with ID: " + userId);
 
         return userRepository.findById(userId).orElseThrow();
     }
 
     public void deleteUserById(Long userId) {
-        loggingService.logInfo("delete user with ID: " + userId);
+        logHandler.logInfo("delete user with ID: " + userId);
 
         userRepository.deleteById(userId);
     }
--- hello-module/src/main/java/com/simple/hellomodule/infrastructure/LogHandler.java
+++ hello-module/src/main/java/com/simple/hellomodule/infrastructure/LogHandler.java
@@ -1,13 +1,13 @@
-package com.simple.hellomodule.common;
+package com.simple.hellomodule.infrastructure;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 @Component
-public class LoggingService {
+public class LogHandler {
 
-    private static final Logger LOGGER = LogManager.getLogger(LoggingService.class);
+    private static final Logger LOGGER = LogManager.getLogger(LogHandler.class);
 
     public void logInfo(String message) {
         LOGGER.info(message);
